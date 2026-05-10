3d795da1def16f944f67441ebad80f32f72a3611
RyanJFM <ryanjffernando@gmail.com>
2026-05-10 10:13:17 +0530

    lec3

---
+++ lecture3/tasks/views.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from django.shortcuts import render
+
+tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Task")
+
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+def add(request):
+    return render(request, "tasks/add.html", {
+        "form":NewTaskForm()
+    })
+
